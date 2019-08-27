8a650eeecc0b7ceea1e75ebc67124fc959f06d85
ssli23 <43375558+ssli23@users.noreply.github.com>
2019-08-27 20:11:15 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,31 +1,8 @@
-# =============================================================================
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 
-"""Routine for decoding the NUS-WIDE binary file format."""
 
 import os
 import cv2
 import numpy as np
-
-
-# Process images of this size. Note that this differs from the original nus-wide
-# image size of 224 x 224. If one alters this number, then the entire model
-# architecture will change and any model would need to be retrained.
-
-# Global constants describing the NUS-WIDE data set.
-
 
 class Dataset(object):
     def __init__(self, modal, data_root, path, train=True):
